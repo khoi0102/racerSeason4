30cc4b93631b5127af237b4335a71c5417f70195
Khoi <lethanhkhoi01@gmail.com>
2025-03-12 13:02:20 -0500

    Update health.js

--- api/health.js
+++ api/health.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',function(req,res){
-    res.send(' Check');
+    res.send(' Ok');
 })
 
 module.exports = router;
